a839afba4479f7485302d2fabc9341630889ea7c
maywang1214 <112986613+maywang1214@users.noreply.github.com>
2023-09-12 14:08:05 -0700

    2.7 Player Control Completed

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,10 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/********************************************
+ * Component of the Main Camera that keeps it
+ * a constant distance from the vehicle
+ * 
+ * May Wang
+ * September 11, 2023 Version 1.0
+ *******************************************/
+
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] private Transform vehicleTransform;
-    private Vector3 offset;
+    [Tooltip("Drag Vehicle onto Vehicle Transform")]
+    [SerializeField] private Transform vehicleTransform; // to keep track of vehicle's position
+    private Vector3 offset;                              //distance camera is from vehicle
 
     // Start is called before the first frame update
     void Start()
@@ -13,7 +22,7 @@ public class CameraController : MonoBehaviour
         offset = new Vector3(0, 5, -7);
     }
 
-    // Update is called once per frame
+    // Update is called once per frame just after Update
     void LateUpdate()
     {
         transform.position = vehicleTransform.position + offset;
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,19 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
+/********************************************
+ * Component of the Vehicle, takes in user
+ * imput to move and thurn the vehicle
+ * 
+ * May Wang
+ * September 11, 2023 Version 1
+ *******************************************/
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 20.0f;
-    // Start is called before the first frame update
+    private float speed;            // holds the forward movement of the vehicle
+    private float turnSpeed;        // holds the turn speed of the vehicle
+    private float verticalInput;    // gets a value [-1, 1] from user key press up/down or W/S
+    private float horizontalInput;  // gets a value [-1, 1] from user key press left/right A/S
+
+    private Rigidbody rb;           // points to vehicle rigidbody component
+
+    // Initializes speed before the first frame update
     void Start()
     {
-        
+        speed = 20.0f;
+        turnSpeed = 50.0f;
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        rb.AddRelativeForce(Vector3.forward * speed * verticalInput * 500);
+        transform.Rotate(Vector3.up * turnSpeed * horizontalInput * Time.deltaTime);
+    }
+
+    // Called from PlayerActionInput when user presses WASD or arrow keys
+    private void OnMove(InputValue input)
+    {
+        verticalInput = input.Get<Vector2>().y;
+        horizontalInput = input.Get<Vector2>().x;
     }
 }
